d6dc3fa1570b57f3d0a240dcd8dacb8871ff575d
Thomas Miller <thomas.miller3@tesco.com>
2019-08-03 22:41:09 +0100

    Add a executor as there is IO bound work

--- src/main/java/thomasmillergb/service/StreamingService.java
+++ src/main/java/thomasmillergb/service/StreamingService.java
@@ -9,20 +9,25 @@ import thomasmillergb.model.TransactionResultRx;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class StreamingService implements Service {
 
 
     private final Database database;
 
+    private final Executor executor;
 
     public StreamingService(final Database database) {
         this.database = database;
+        //Set to 200 as the db is IO bound
+        executor = Executors.newFixedThreadPool(200);
     }
 
     @Override
     public CompletionStage<Optional<String>> processTransaction(final Transaction tx) {
-        return CompletableFuture.supplyAsync(() -> database.saveTransaction(tx))
+        return CompletableFuture.supplyAsync(() -> database.saveTransaction(tx), executor)
                 .thenApply(TransactionResult::getErrorMessage);
 
     }
@@ -30,6 +35,7 @@ public class StreamingService implements Service {
     @Override
     public Observable<TransactionResultRx> processTransactionRx(final Transaction tx) {
         return Observable.just(tx)
+                //So much nicer
                 .observeOn(Schedulers.io())
                 .map(database::saveTransactionRx);
     }
